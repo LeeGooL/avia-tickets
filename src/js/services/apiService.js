2f84dc6f2071fa84b8ace00891a9f3f3d34f6965
GooJoke <efsvags@icloud.com>
2021-01-13 13:27:41 +0300

    Form functionality implementation

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -30,6 +30,18 @@ class Api {
       return Promise.reject(error);
     }
   }
+
+  async prices(params) {
+    try {
+      const response = await axios.get(`${this.url}/prices/cheap`, { params });
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
+
+      return Promise.reject(error);
+    }
+  }
 }
 
 const api = new Api(config);
--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
     this.api = api;
     this.countries = null;
     this.cities = null;
+    this.shortCitiesList = {};
   }
 
   async init() {
@@ -15,16 +16,59 @@ class Locations {
 
     const [countries, cities] = response;
 
-    this.countries = countries;
-    this.cities = cities;
+    this.countries = this.serializeCountries(countries);
+    this.cities = this.serializeCities(cities);
+    this.shortCitiesList = this.createShortCitiesList(this.cities);
 
     return response;
   }
 
-  getCityNameByCode(code) {
-    return this.cities.filter((city) => {
-      return city.country_code === code;
-    });
+  getCityNameByCountryCode(code) {
+    return this.countries[code].name;
+  }
+
+  getCityCodeByKey(key) {
+    return this.cities[key].code;
+  }
+
+  serializeCountries(countries) {
+    // { 'Country code': {...} }
+
+    return countries.reduce((acc, country) => {
+      acc[country.code] = country;
+
+      return acc;
+    }, {});
+  }
+
+  serializeCities(cities) {
+    // { 'City name, Country name': {...} }
+
+    return cities.reduce((acc, city) => {
+      const country_name = this.getCityNameByCountryCode(city.country_code);
+      const city_name = city.name || city.name_translations.en;
+      const full_name = `${city_name}, ${country_name}`;
+
+      acc[full_name] = { ...city, country_name, city_name, full_name };
+
+      return acc;
+    }, {});
+  }
+
+  createShortCitiesList(cities) {
+    // { 'City, Country': null }
+
+    return Object.entries(cities).reduce((acc, [city]) => {
+      acc[city] = null;
+
+      return acc;
+    }, {});
+  }
+
+  async fetchTickets(params) {
+    const response = await this.api.prices(params);
+
+    console.log(response.data);
   }
 }
 
